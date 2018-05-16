00dddea9fb7285ae433fc4fc77d04c1b1edbeec8
María Luena Rodríguez <maluenarod@gmail.com>
2018-05-16 12:07:41 +0200

    adding spaces when character isnot specified test

--- paddingLeft.js
+++ paddingLeft.js
@@ -1,20 +1,26 @@
 function paddingLeft(initialString,stringLength,character){
-    
+    let characters = stringLength - initialString.length;
+  
     if (stringLength <= initialString.length){
         return initialString;
     }
     else if (stringLength > initialString.length){
+        if (!character){
+            character = ' ';
+        }
         let finalAdding = '';
-        let characters = stringLength - initialString.length;
         for (let i = 0; i < characters; i++){
             finalAdding = finalAdding + character;
          }
          return finalAdding + initialString;
     }
-
   }
   module.exports = paddingLeft;
 
+
+
+
+// module.exports = paddingLeft;
 
 //   Crear una función padding-left que se encarga de meter caracteres de relleno en un cadena por el lado izquierdo hasta llegar a un tamaño deseado. Toma 3 parámetros
 
--- paddingLeft.test.js
+++ paddingLeft.test.js
@@ -1,26 +1,21 @@
 const paddingLeft = require('./paddingLeft.js');
 describe('PaddingLeft', () => {
-  test('Adds x characters at the beginning', () => {
-    const finalString = 'xxhola';
-
+  test('Adds 1, 6, 11 or 16 spaces at the beginning of my string if we dont specify any character and the length is 5, 10, 15 or 20 respectively', () => {
+    expect(paddingLeft('hola', 5)).toBe(' hola'); 
+    expect(paddingLeft('hola', 10)).toBe('      hola'); 
+    expect(paddingLeft('hola', 15)).toBe('           hola'); 
+    expect(paddingLeft('hola', 20)).toBe('                hola'); 
+  });
+  test('Adds 2, 5, 8 or 11 "x" at the beginning of my string if the specified length is 6, 9, 12 or 15', () => {
     expect(paddingLeft('hola', 6, 'x')).toBe('xxhola'); 
-  });
-  test('Returns same string if string length is 3', () => {
-    const initialString = 'hola';
-    
-    const stringLength = 3;
-
-    const character = 'x';
-
-    expect(paddingLeft('hola', 3, 'x')).toBe('hola'); 
-  });
-  test('Adds xxxxxxxx at the beginnig if string length is 12', () => {
-    const initialString = 'hola';
-    
-    const stringLength = 12;
-
-    const character = 'x';
-
+    expect(paddingLeft('hola', 9, 'x')).toBe('xxxxxhola'); 
     expect(paddingLeft('hola', 12, 'x')).toBe('xxxxxxxxhola'); 
+    expect(paddingLeft('hola', 15, 'x')).toBe('xxxxxxxxxxxhola'); 
+  });
+  test('Returns same string if the string length is 3, 2, 1 or 0', () => {
+    expect(paddingLeft('hola', 3, 'x')).toBe('hola'); 
+    expect(paddingLeft('hola', 2, 'x')).toBe('hola'); 
+    expect(paddingLeft('hola', 1, 'x')).toBe('hola'); 
+    expect(paddingLeft('hola', 0, 'x')).toBe('hola');
   });
 });
